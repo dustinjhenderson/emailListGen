509579b980071dee0a783a41417136ad4dfea00b
edemir1 <eates1@binghamton.edu>
2017-09-11 16:28:06 -0700

    Final

--- main.py
+++ main.py
@@ -4,20 +4,26 @@
 
 import csv		#imports libraries used to read the csv files
 #import string
-
 from append import append
 from parsCsv import parsCsv
+import Tkinter,tkFileDialog,tkMessageBox  #Used Tkinter library for creating the GUI
+from tkFileDialog import askopenfilename
+from Tkinter import *
 
-#listFromCsv = []
-csvLocation = "testFile1.csv"
 
-print "CSV open"
+root = Tkinter.Tk()
+T = Text(root, height=6, width=80)
+T.pack()
+T.insert(END, "Welcome to email generator!\n\n\n\nPlease upload your file and select a directory for your updated email address.\n")
+T.tag_add("color1", "1.0", "10.0")
+T.tag_config("color1",background="black",foreground="green")
 
-listFromCsv = parsCsv(csvLocation) 
+fileName = tkFileDialog.askopenfilename()
+if fileName != "":
+	tkMessageBox.showinfo("Confirmation", "Your file is uploaded successfully!")
+	csvLocation = fileName
+	listFromCsv = parsCsv(csvLocation)
+	append(listFromCsv)
+if fileName == "":
+	 root.destroy()
 
-for line in listFromCsv:
-	print line
-
-print "File Open"
-	
-append(listFromCsv)
