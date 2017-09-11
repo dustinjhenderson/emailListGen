37dd71f9d88fec1d4ef2bd42735d2b6abf15c2a4
edemir1 <eates1@binghamton.edu>
2017-09-11 11:13:16 -0700

    Final version of append function

--- append.py
+++ append.py
@@ -1,24 +1,13 @@
+#This function reads the list of emails from cvs file and creates a text file with the updated email list
 
-def append(cvs_list):
-	#cvs_list=["ezgi@gmail.com", "dustin@gmail.com"];
+def append(cvs_list): #gets list of emails from cvs file
 	email_list = []
 	file = open('test.txt', 'r') #read existing email file
 	file1=file.read()
-	print "email list before: "
-	print file1
-	email_list = file1.split(';') #create email list file
+	email_list = file1.split(';') #create a list from email file
 	
 	for i in cvs_list:
-	  if i not in email_list:
+	  if i not in email_list:  #if the email is not added before
 		email_list.append(i)
-	print "email_list: "
-	print email_list
-	with open ('test.txt', 'w') as out:
-		out.write(';'.join([str(n) for n in email_list]))
-  
-
-
-
-
-
-
+	with open ('test.txt', 'w') as out:  #update the email list
+		out.write(';'.join([str(n) for n in email_list])) #create the updated text file
